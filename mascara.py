589689f680011c895ef4355e1c633e44fdab441c
Diego Torres <diegopaivatorres@gmail.com>
2024-02-29 15:17:15 -0400

    obtendo vizinhos

--- mascara.py
+++ mascara.py
@@ -2,8 +2,8 @@ from PIL import Image
 
 imagem_original = Image.open("imagem_desgaste.jpg")
 pixels = imagem_original.load()
-tam_vizi = (4,4)
-
+tam_vizi = (1,1)
+vizinhos = []
 larg, altu = imagem_original.size
 
 def vizinhanca():
@@ -11,10 +11,26 @@ def vizinhanca():
     for x in range(larg):
         for y in range(altu):
             #obtendo os vizinhos
-            for i in range(tam_vizi[0]):
-                for j in range(tam_vizi[1]):
-                    # pixels[x,y] = ()
-                    print(i, j)
+            try:
+                for i in range(tam_vizi[0]):
+                    for j in range(tam_vizi[1]):
+                        # pixels[x,y] = ()
+                        vizinhos.append(pixels[x+j+1, y+j])
+                        vizinhos.append(pixels[x + i + 2, y + j + 1])
+                        vizinhos.append(pixels[x + i + -1, y + j + 1])
+                        vizinhos.append(pixels[x + i + 1, y + j + 1])
+                        vizinhos.append(pixels[x + i + 1, y + j + 1])
+                        vizinhos.append(pixels[x + i + 1, y + j + 1])
+                        vizinhos.append(pixels[x + i + 1, y + j + 1])
+                        vizinhos.append(pixels[x + i + 1, y + j + 1])
+
+                    print(pixels[x,y], "Vizinho:", pixels[x+i+1, y+j+1])
+            except:
+                pass
+
+                    # print(vizinhos)
+            # print(len(vizinhos))
+            vizinhos.clear()
 
             # r,g,b = pixels[x,y]
 
